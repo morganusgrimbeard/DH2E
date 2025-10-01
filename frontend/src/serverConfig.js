1e9a63f29e5344aaeaee3b83d92c9b70c78ef16e
morganusgrimbeard <morganusgrimbeard@gmail.com>
2025-10-01 22:10:10 +0100

    Update serverConfig.js

--- frontend/src/serverConfig.js
+++ frontend/src/serverConfig.js
@@ -1,7 +1,7 @@
 // serverConfig.js
 // Central config for backend URLs
 
-const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+const serverUrl = import.meta.env.VITE_SERVER_URL || "https://dh2e.onrender.com";
 const wsUrl = serverUrl.replace(/^http/, "ws");
 
 export { serverUrl, wsUrl };
